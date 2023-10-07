270a3bd3f43ea8def8b2e742f003ce9803a3ccad
NguyenDat-2003 <maxdat2003@gmail.com>
2023-10-07 12:34:05 +0700

    #18 Config Template (View) Engine

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 5050;
+
+// config template engine
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// khai báo route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/new", (req, res) => {
+  // res.send("Check new page!");
+  res.render("sample.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
