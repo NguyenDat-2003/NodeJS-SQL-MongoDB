fddabc3645bf85b7019951c8a68827d0706c50a2
NguyenDat-2003 <maxdat2003@gmail.com>
2023-10-07 12:48:55 +0700

    #19. ENV (Environment Variables)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
-const port = 5050;
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
 
 // config template engine
 app.set("views", path.join(__dirname, "views"));
@@ -18,6 +20,6 @@ app.get("/new", (req, res) => {
   res.render("sample.ejs");
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
