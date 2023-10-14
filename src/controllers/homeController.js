cffd6a839e04fe7ad338fca1ea3704eefd34e586
NguyenDat-2003 <maxdat2003@gmail.com>
2023-10-14 21:10:54 +0700

    #33. Connection Pool Pattern

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,11 @@
+const connection = require("../config/database");
+
 const getHomePage = (req, res) => {
-  res.send("Hello World with pakage Nodemon sdasd!");
+  let users = [];
+  connection.query("SELECT * FROM Users ", function (err, results, fields) {
+    console.log(">>> results = ", results); // results contains rows returned by server
+    res.send(JSON.stringify(users));
+  });
 };
 const getNewPage = (req, res) => {
   res.render("sample.ejs");
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-const mysql = require("mysql2");
+const connection = require("./config/database");
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -15,17 +15,9 @@ configViewEngine(app);
 app.use("/", webRoutes);
 
 //test connection with database
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307, //default là cổng 3306
-  user: "root",
-  password: "123456",
-  database: "hoidanit",
-});
 
 connection.query("SELECT * FROM Users ", function (err, results, fields) {
   console.log(">>> results = ", results); // results contains rows returned by server
-  console.log(">>> fields = ", fields); // fields contains extra meta data about results, if available
 });
 
 app.listen(port, hostname, () => {
