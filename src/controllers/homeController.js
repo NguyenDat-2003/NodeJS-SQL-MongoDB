1e99987eeebceeae86a8c5bb798037d7d209c941
NguyenDat-2003 <maxdat2003@gmail.com>
2023-10-15 09:26:17 +0700

    #35. Design NavBar

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,14 +1,10 @@
 const connection = require("../config/database");
 
 const getHomePage = (req, res) => {
-  let users = [];
-  connection.query("SELECT * FROM Users ", function (err, results, fields) {
-    console.log(">>> results = ", results); // results contains rows returned by server
-    res.send(JSON.stringify(users));
-  });
+  return res.render("home.ejs");
 };
 const getNewPage = (req, res) => {
-  res.render("sample.ejs");
+  return res.render("sample.ejs");
 };
 
 module.exports = { getHomePage, getNewPage };
