3b9610eeb3aeaad5eca8090a43a7ff90fced6d49
NguyenDat-2003 <maxdat2003@gmail.com>
2023-12-20 10:20:23 +0700

    #38. Chức Năng Create User

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -7,8 +7,22 @@ const getNewPage = (req, res) => {
   return res.render("sample.ejs");
 };
 const postCreateUser = (req, res) => {
-  console.log(">> check req: ", req.body);
-  res.send("Create new user");
+  // let email = req.body.email;
+  // let name = req.body.name;
+  // let city = req.body.city;
+
+  let { email, name, city } = req.body;
+
+  connection.query(
+    // Truyền động data từ form đến db bằng dấu ?
+    `INSERT INTO Users(email, name, city) 
+    VALUES(?, ?, ?)`,
+    [email, name, city],
+    function (err, results) {
+      console.log(">>> results = ", results); // results contains rows returned by server
+      res.send("Create user successeed");
+    }
+  );
 };
 
 module.exports = { getHomePage, getNewPage, postCreateUser };
